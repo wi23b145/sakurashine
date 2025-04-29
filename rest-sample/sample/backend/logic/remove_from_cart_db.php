43d0ee5f2865c83fcbc157037e816f7bbc7fe9e8
wi23b145 <wi23b145@technikum-wien.at>
2025-04-29 13:45:31 +0200

    Warenkorb Funktionalität teils vorhanden

---
+++ rest-sample/sample/backend/logic/remove_from_cart_db.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+require_once('../config/dbaccess.php');
+
+if (!isset($_SESSION['id'])) {
+    header('Location: ../sites/login.html');
+    exit;
+}
+
+$user_id = $_SESSION['id'];
+$produkt_id = $_POST['produkt_id'];
+
+$sql = "DELETE FROM Warenkorb WHERE user_id = ? AND produkt_id = ?";
+$stmt = $con->prepare($sql);
+$stmt->bind_param("ii", $user_id, $produkt_id);
+$stmt->execute();
+
+header('Location: ../sites/warenkorb.php');
+exit;
+?>
